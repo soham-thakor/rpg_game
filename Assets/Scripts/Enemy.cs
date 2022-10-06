de56ff258816529252daccf58a25c0714ab49c17
Github2287 <98567375+Github2287@users.noreply.github.com>
2022-10-05 22:20:45 -0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,7 +14,7 @@ public class Enemy : MonoBehaviour
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth((int)maxHealth);
-        flasheffect = GameObject.FindGameObjectWithTag("Effect").GetComponent<SimpleFlash>();
+        
     }
     
     // Gets called forom the attack sword.
