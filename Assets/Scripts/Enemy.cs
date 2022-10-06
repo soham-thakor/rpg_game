91e512a700f346f71b63b92db92e8bd56d8f7529
Github2287 <xvictorx588@gmail.com>
2022-10-06 16:40:10 -0700

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,11 @@ public class Enemy : MonoBehaviour
     public SimpleFlash flasheffect;
     public float maxHealth = 100;
     public float currentHealth;
+    public float speed = 2.5f;
+
+
+    Transform player;
+    Rigidbody2D rb;
 
     //public float Health;
     void Start()
@@ -16,7 +21,21 @@ public class Enemy : MonoBehaviour
         healthBar.SetMaxHealth((int)maxHealth);
         
     }
-    
+/*
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = Gameobject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //animator.GetComponent<>
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+  */
     // Gets called forom the attack sword.
     public void TakeDamage(float damage)
     {
