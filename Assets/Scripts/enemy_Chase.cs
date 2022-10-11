d9ddd7b7b688a4258dbe20f76af2d7ee92ec77a6
Collin Newman <collinnewman12@gmail.com>
2022-10-11 12:30:39 -0500

    update chase script

--- Assets/Scripts/enemy_Chase.cs
+++ Assets/Scripts/enemy_Chase.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class enemy_Chase : StateMachineBehaviour
 {
     public float speed = 1.0f;
+    public float attackRange = .01f;
+    public float chaseRange = .5f;
+
+    private bool near=false;
     private float offset;
     private Transform player;
     private Rigidbody2D rb;
@@ -21,15 +25,28 @@ public class enemy_Chase : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+
+        if(Vector2.Distance(player.position, rb.position) <= chaseRange)
+        {
+            near=true;
+        }
         // calculate offset of player position (so enemy doesnt stand inside of player)
         if(player.position.x > 0) { offset = 0.2f; }
         else { offset = -0.2f; }
         
-        Vector2 target = new Vector2(player.position.x+offset, player.position.y);
+        if(near){
+            Vector2 target = new Vector2(player.position.x+offset, player.position.y);
 
-        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+            Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
 
-        rb.MovePosition(newPos);
+            rb.MovePosition(newPos);
+        }
+
+        // if(Vector2.Distance(player.position, rb.position) <= attackRange)
+        // {
+        //     animator.SetTrigger("Attack");
+        // }
+        
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -37,6 +54,4 @@ public class enemy_Chase : StateMachineBehaviour
     {
         
     }
-
-   
 }
