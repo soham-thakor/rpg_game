c851bb0e8d5cb465b83980f45cb1c5cc52a8a597
manmythmardo <alexc.mardoqueo@gmail.com>
2022-11-10 16:16:31 -0600

    Pause menu added and Light changes to Dungeon

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+
+    private bool isPaused;
+    public GameObject pausePanel;
+    public string mainMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePanel.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("pause"))
+        {
+            ChangePause();
+        }
+        
+    }
+
+    public void ChangePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene(mainMenu);
+        Time.timeScale = 1f;
+    }
+}
