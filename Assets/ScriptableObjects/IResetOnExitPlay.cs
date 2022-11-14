35e7118a23c797fc10a8ef7106f44e7e940f57d6
Collin <collinnewman12@gmail.com>
2022-11-13 19:50:04 -0600

    reset sceneChanged bool when exiting play mode

---
+++ Assets/ScriptableObjects/IResetOnExitPlay.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+ 
+static class SOPlayModeResetter
+{
+    [InitializeOnLoadMethod]
+    static void RegisterResets()
+    {
+        EditorApplication.playModeStateChanged += ResetSOsWithIResetOnExitPlay; 
+    }
+
+    static void ResetSOsWithIResetOnExitPlay(PlayModeStateChange change)
+    {
+        if (change == PlayModeStateChange.ExitingPlayMode)
+        {
+            var assets = FindAssets<ScriptableObject>();
+            foreach (var a in assets)
+            {
+                if (a is IResetOnExitPlay)
+                {
+                    (a as IResetOnExitPlay).ResetOnExitPlay();
+                }
+            }
+        }
+    }
+
+    static T[] FindAssets<T>() where T : Object
+    {
+        var guids = AssetDatabase.FindAssets($"t:{typeof(T)}");
+        var assets = new T[guids.Length];
+        for (int i = 0; i < guids.Length; i++)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guids[i]);
+            assets[i] = AssetDatabase.LoadAssetAtPath<T>(path);            
+        }
+        return assets;
+    }
+}
+#endif
+
+public interface IResetOnExitPlay
+{
+    public void ResetOnExitPlay();
+}
--- Assets/ScriptableObjects/SceneChange.cs
+++ Assets/ScriptableObjects/SceneChange.cs
@@ -3,38 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class SceneChange : ScriptableObject
+public class SceneChange : ScriptableObject, IResetOnExitPlay
 {
     public Vector2 initialValue;
     public bool movedScene = false;
 
-    public void OnBeforeSerialize() 
+    public void ResetOnExitPlay()
     {
-        Debug.Log("onBeforeDeserialize");
-        movedScene=false;
-    }
-
-    public void Awake() {
-        Debug.Log("awake");
         movedScene = false;
-    }
-
-    private void OnEnable()
-    {
-        Debug.Log("on enable");
-        movedScene = false;
-    }
-
-    private void OnDisable()
-    {
-        Debug.Log("on disable");
-        movedScene = false;
-    }
-
-    // You can also use OnAfterDeserialize for the other way around
-    public void OnAfterDeserialize() 
-    {
-        Debug.Log("onAfterDeserialize");
-        movedScene=false;
-    }
+    }    
+    
 }
