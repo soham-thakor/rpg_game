e6e776aed4a11b4f2b1d12b2cf67115bad7f2c3c
manmythmardo <alexc.mardoqueo@gmail.com>
2022-11-26 01:21:53 -0600

    Quest Tracker and Manager Updated

--- Assets/Scripts/Dialogue/Quest.cs
+++ Assets/Scripts/Dialogue/Quest.cs
@@ -8,37 +8,48 @@ public class Quest : MonoBehaviour
     public GameObject dialogBox;
     public Text dialogText;
     public Message [] messages;
+    public NPC actor;
+
     public int currentQuest;
     private int questTracker = 0;
     private bool playerInRange;
-    private int activemsg = 0;
-
+    private int cuMsg = 0;
+    public QuestTrackerData data1;
+    
     void Start()
     {
         dialogBox.SetActive(false);
     }
 
-
     void Update(){
-        if(Input.GetKeyDown(KeyCode.E)){
+        if(Input.GetKeyDown(KeyCode.E) && (currentQuest < 2)){
             currentQuest++;
         }
+
         if(questTracker != currentQuest){
             questTracker = currentQuest;
-            activemsg = 0;
+            cuMsg = 0;
         }
         if(Input.GetKeyDown(KeyCode.F) && playerInRange){
+            if(data1.npcTalked(actor.name) == 0 && actor.id == 1){
+                data1.interactions++;
+                data1.interact[actor.name]++;
+            }else if(data1.npcTalked(actor.name) >= 0 && actor.id == 2){
+                data1.interactions++;
+                data1.interact[actor.name]++;
+            }
+
             SoundManager.PlaySound(SoundManager.Sound.DialogueSound);
             
             if(dialogBox.activeInHierarchy){
                 dialogBox.SetActive(false);
             }else{
                 dialogBox.SetActive(true);
-                string msgToDisplay = messages[questTracker].message[activemsg];
+                string msgToDisplay = messages[questTracker].message[cuMsg];
                 dialogText.text = msgToDisplay;
 
-                if(activemsg < messages[questTracker].message.Length-1){
-                    activemsg++;
+                if(cuMsg < messages[questTracker].message.Length-1){
+                    cuMsg++;
                 }
             }
         }
@@ -48,6 +59,13 @@ public class Quest : MonoBehaviour
     public class Message{
         public string [] message;
     }
+
+    [System.Serializable]
+    public class NPC{
+        public string name;
+        public int id;
+    }
+
     public void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
             playerInRange = true;
