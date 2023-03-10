6b3d8ed5e7e429542b70c2c2070fef4bdc9fa099
Collin Newman <collinnewman12@gmail.com>
2023-03-09 23:06:50 -0600

    make player invincible during fire heal

--- Assets/Scripts/Player/Heal.cs
+++ Assets/Scripts/Player/Heal.cs
@@ -8,24 +8,18 @@ public class Heal : MonoBehaviour
     public float lifetime = 1f;
 
     private PlayerController player;
-    private float beforeCastHealth;
 
     void OnEnable()
     {
         player = FindObjectOfType<PlayerController>().GetComponent<PlayerController>();
+        staticVariables.invincible = true;
         Invoke("DisableHeal", lifetime);
-        beforeCastHealth = player.currentHealth;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        player.currentHealth = beforeCastHealth; // makes player invulnerable
     }
 
     private void DisableHeal()
     {
+        staticVariables.invincible = false;
         player.AddHealth(healAmount);
-        gameObject.SetActive(false);
+        Destroy(gameObject);
     }
 }
